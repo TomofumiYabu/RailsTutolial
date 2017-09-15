b6308c19215724d7396e7b3d4ef0ef14377bd063
Tomofumi Yabu <say77@hotmail.com>
2017-09-15 05:48:13 +0000

    Add create method to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #保存成功時の処理を後で書く
+    else
+      render 'new'
+    end
+  end
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
