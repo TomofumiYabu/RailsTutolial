c524c01b8ab257efef9c3a3477a27bb406994eda
Tomofumi Yabu <say77@hotmail.com>
2017-09-19 07:57:48 +0000

    Add Test for SignUp Error Message

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select 'div.field_with_errors'
+    assert_select 'ul' do
+      assert_select 'li', 'Name can\'t be blank'
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', 'Password confirmation doesn\'t match Password'
+      #パスワードの文字数制限が変わった時のため、文字数部分のエラーメッセージより前で部分一致を検証
+      assert_select 'li', /Password is too short*/
+    end
   end
 end
