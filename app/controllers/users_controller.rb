3f7b5e36bd06fe48aa37671ef3d8001e2bdd62c6
Tomofumi Yabu <say77@hotmail.com>
2017-10-05 05:49:23 +0000

    Add before action for edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
-        # beforeアクション
+    # beforeアクション
 
     # ログイン済みユーザーかどうか確認
     def logged_in_user
