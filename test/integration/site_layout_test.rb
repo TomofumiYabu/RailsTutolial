56f744dd2bcc502e9f6a86355786b32e75bd6c68
Tomofumi Yabu <say77@hotmail.com>
2017-10-10 05:26:10 +0000

    add index page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,4 +18,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+  
+  test "layout links for logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+  end
+  
+  test "layout links for not logged in user" do
+    log_in_as(@user)
+    delete logout_path  #一度ログアウトする
+    get root_path
+    assert_select "a[href=?]", users_path, false
+  end
+  
 end
