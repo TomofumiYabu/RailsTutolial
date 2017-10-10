6c0b93e702c0a681ac32422b934f5f1fb8e56f46
Tomofumi Yabu <say77@hotmail.com>
2017-10-10 06:16:38 +0000

    add faker to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+             
+User.create!(name:  "Say",
+             email: "say77@hotmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+User.create!(name:  "Test",
+             email: "test@hotmail.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+100.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
