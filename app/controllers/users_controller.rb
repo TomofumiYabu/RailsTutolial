7822b0cff2a48640939d05e3578f34ba7c549f3f
Tomofumi Yabu <say77@hotmail.com>
2017-11-02 05:36:02 +0000

    Add Micropost view to User show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
-    # @users = User.paginate(page: params[:page])
   end
   
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
