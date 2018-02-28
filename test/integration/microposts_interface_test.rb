503fe333bcda9abc517f20574f632fb81394f239
Tomofumi Yabu <say77@hotmail.com>
2018-02-28 08:24:17 +0000

    Add test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -34,4 +34,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count:0
   end
   
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    number_microposts = @user.microposts.count.to_s
+    assert_match number_microposts + " microposts", response.body
+  end
+  
+  test "micropost sidebar count for user posted no micropost" do
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "テスト")
+    get root_path
+    assert_match "1 micropost", response.body
+  end
 end
