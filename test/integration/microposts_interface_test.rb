5413bb95d97a4b6610f2eab5dfb64244d2922165
Tomofumi Yabu <say77@hotmail.com>
2018-03-07 08:29:43 +0000

    Add Picture Upload Function

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination' #Homeにページネーションはあるか？
+    assert_select 'input[type=file]' #ファイル投稿ボタンはあるか？
     #無効な送信
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,9 +18,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     #有効な送信
     words = "テスト文章"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count' , 1 do
-      post microposts_path, params: { micropost: { content: words } }
+      post microposts_path, params: { micropost: { content: words, picture: picture } }
     end
+    assert @user.microposts.first.picture? 
     assert_redirected_to root_url
     follow_redirect!
     assert_match words, response.body
