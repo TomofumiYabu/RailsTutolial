36e59164f171c440156615b53d5008a9e14f969d
Tomofumi Yabu <say77@hotmail.com>
2018-03-15 02:49:52 +0000

    Add DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#Users
 User.create!(name:  "Admin User",
              email: "admin@test.com",
              password:              "foobar",
@@ -52,8 +53,20 @@ User.create!(name:  "Invalid",
                activated_at: Time.zone.now)
 end
 
+#Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content)}
 end
+
+#Relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
+
+
